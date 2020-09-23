6093994bfbac8cf346d3d2ea2822aff45c2ab56b
Matt Bugay <mjbugay@yahoo.com>
2020-09-23 04:10:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,17 @@ def load_library(path)
   results
 end
   
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
+  library = load_library(path)
+  
+  library.each do |key, value|
+    if value[:english] == emoticon
+      return value[:japanese]
+    end
+  end
+  
+  return "Sorry, that emoticon was not found"
+end
 
 end
 
